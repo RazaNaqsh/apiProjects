04169a913254e2c04f6f83377e2eea28d0ec58b1
RazaNaqsh <owaisraza1704@gmail.com>
2023-02-19 22:58:27 +0530

    Promise based code changed to Async await code

--- giphySearch/script.js
+++ giphySearch/script.js
@@ -8,22 +8,32 @@ let search = "cat";
 
 input.value = search;
 
-function fetchImage(search) {
+async function fetchImage(search) {
 	// console.log(search);
-	fetch(
+
+	// promise based code
+	// fetch(
+	// 	`https://api.giphy.com/v1/gifs/translate?api_key=bb2006d9d3454578be1a99cfad65913d&s=${search}`,
+	// 	{ mode: "cors" }
+	// )
+	// 	.then(function (response) {
+	// 		return response.json();
+	// 	})
+	// 	.then(function (response) {
+	// 		img.src = response.data.images.original.url;
+	// 	})
+	// 	.catch((e) => {
+	// 		img.textContent = e;
+	// 		console.log(e);
+	// 	});
+
+	// async await code
+	const response = await fetch(
 		`https://api.giphy.com/v1/gifs/translate?api_key=bb2006d9d3454578be1a99cfad65913d&s=${search}`,
 		{ mode: "cors" }
-	)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (response) {
-			img.src = response.data.images.original.url;
-		})
-		.catch((e) => {
-			img.textContent = e;
-			console.log(e);
-		});
+	);
+	const imgData = await response.json();
+	img.src = imgData.data.images.original.url;
 }
 fetchImage(search);
 
